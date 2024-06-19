5526887acca51bfac6861791ea6c52f149d5205b
Dohyun Yoo <do390692d@gmail.com>
2024-06-19 23:06:33 +0900

    a

--- app/src/main/java/com/cookandroid/project_diary/CheckPassword.java
+++ app/src/main/java/com/cookandroid/project_diary/CheckPassword.java
@@ -16,7 +16,7 @@ import java.io.IOException;
 
 public class CheckPassword extends AppCompatActivity {
     String id, pwd;
-    Button btnCheckPwd;
+    Button btnCheckPwd, btnBack;
     EditText edtCheckPwd;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState){
@@ -24,47 +24,71 @@ public class CheckPassword extends AppCompatActivity {
         setContentView(R.layout.checked_password);
 
         btnCheckPwd = (Button) findViewById(R.id.btnCheckPwd);
+        btnBack = (Button) findViewById(R.id.btnBack);
         edtCheckPwd = (EditText) findViewById(R.id.edtCheckPwd);
 
         // 아이디 전달받기
         id = getIntent().getStringExtra("ID");
         File folder = new File(getFilesDir(), "ID/" + id);
-        File file = new File(getFilesDir(), "ID/" + id + "/" + id +".txt");
 
-        if(file.exists()) {
-            pwd = readPwd(file.toString());
-        }else{
-            Toast.makeText(getApplicationContext(), "아이디가 존재하지 않습니다.",
-                    Toast.LENGTH_SHORT).show();
-        }
+        pwd = readPassword(id);
 
         btnCheckPwd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(pwd.equals(edtCheckPwd.toString())){
-                    boolean delete = folder.delete();
-                    Intent intent = new Intent(CheckPassword.this,
-                            MainActivity.class);
+                String enteredPwd = edtCheckPwd.getText().toString();
+                if(pwd.equals(enteredPwd)){
+                    // 비밀번호 일치 시 폴더 내 파일들을 모두 삭제하고 폴더를 삭제
+                    deleteFolderContentAndFolder(folder);
+
+                    // MainActivity로 이동
+                    Intent intent = new Intent(CheckPassword.this, MainActivity.class);
                     startActivity(intent);
-                }else{
-                    Toast.makeText(getApplicationContext(), "비밀번호가 틀립니다.",
-                            Toast.LENGTH_SHORT).show();
+                    finish(); // 현재 액티비티 종료
+                } else {
+                    Toast.makeText(getApplicationContext(), "비밀번호가 틀립니다.", Toast.LENGTH_SHORT).show();
                 }
+            }
+        });
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CheckPassword.this,
+                        BasicScreen.class);
+                intent.putExtra("ID", id);
+                startActivity(intent);
             }
         });
 
     }
-    String readPwd(String fName){
-        String todoStr = null;
+    String readPassword(String ID){
+        String password = null;
         FileInputStream inFs;
-        try{
-            inFs = new FileInputStream(fName);
-            byte[] txt = new byte[3000];
-            inFs.read(txt);
+        try {
+            File file = new File(getFilesDir() + "/ID" + "/" + ID, ID + ".txt");
+            inFs = new FileInputStream(file);
+            byte [] pwd = new byte[100];
+            inFs.read(pwd);
             inFs.close();
-            todoStr = (new String(txt));
+            password = (new String(pwd)).trim();
         }catch (IOException ignored){
         }
-        return todoStr;
+        return password;
+    }
+    // 폴더 내 파일들을 모두 삭제하고 폴더도 삭제하는 메서드
+    private void deleteFolderContentAndFolder(File folder) {
+        if (folder.isDirectory()) {
+            File[] files = folder.listFiles();
+            if (files != null) {
+                for (File file : files) {
+                    if (file.isDirectory()) {
+                        deleteFolderContentAndFolder(file); // 재귀적으로 폴더 내 파일들 삭제
+                    } else {
+                        file.delete(); // 파일 삭제
+                    }
+                }
+            }
+        }
+        folder.delete(); // 폴더 삭제
     }
 }
